35a3decf47a61054f7dab448cb036d30309b2f32
ntpan04 <nguyenan19101112@gmail.com>
2024-10-05 12:51:52 +0700

    crud prescription record

---
+++ KoiFishCare/Dtos/PrescriptionRecord/PrescriptionRecordDto.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoiFishCare.Dtos.PrescriptionRecord
+{
+    public class PrescriptionRecordDto
+    {
+        public int PrescriptionRecordID { get; set; }
+        
+        public string? DiseaseName { get; set; }
+
+        public string? Symptoms { get; set; }
+
+        public string? Medication { get; set; }
+
+        public string? Note { get; set; }
+
+        public decimal? RefundMoney { get; set; }
+
+        public int? RefundPercent { get; set; }
+
+        public int? BookingID { get; set; }
+    }
+}
---
+++ KoiFishCare/Repository/PrescriptionRecordRepository.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KoiFishCare.Data;
+using KoiFishCare.Interfaces;
+using KoiFishCare.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KoiFishCare.Repository
+{
+    public class PrescriptionRecordRepository : IPrescriptionRecordRepository
+    {
+        private readonly KoiFishVeterinaryServiceContext _context;
+        public PrescriptionRecordRepository(KoiFishVeterinaryServiceContext context)
+        {
+            _context = context;
+        }
+        public async Task<PrescriptionRecord?> Create(PrescriptionRecord model)
+        {
+            var bookingExist = await _context.Bookings.AnyAsync(b => b.BookingID == model.BookingID);
+            if (!bookingExist) return null;
+            await _context.PrescriptionRecords.AddAsync(model);
+            await _context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<PrescriptionRecord?> Delete(int id)
+        {
+            var model = await _context.PrescriptionRecords.FirstOrDefaultAsync(p => p.PrescriptionRecordID == id);
+            if (model == null) return null;
+            _context.Remove(model);
+            await _context.SaveChangesAsync();
+            return model;
+        }
+
+        public async Task<PrescriptionRecord?> GetById(int id)
+        {
+            var model = await _context.PrescriptionRecords.FirstOrDefaultAsync(p => p.PrescriptionRecordID == id);
+            return model;
+        }
+
+        public async Task<List<PrescriptionRecord>> GetListByCusUsername(string cusName)
+        {
+            var list = await _context.PrescriptionRecords.Where(p => p.Booking.Customer.UserName.ToLower().Trim() == cusName.ToLower().Trim()).ToListAsync();
+            return list;
+        }
+
+        public async Task<PrescriptionRecord?> Update(int id, PrescriptionRecord model)
+        {
+            var exist = await _context.PrescriptionRecords.FirstOrDefaultAsync(p => p.PrescriptionRecordID == id);
+            if (exist == null) return null;
+
+            exist.DiseaseName = model.DiseaseName;
+            exist.Symptoms = model.Symptoms;
+            exist.Medication = model.Medication;
+            exist.Note = model.Note;
+            exist.RefundMoney = model.RefundMoney;
+            exist.RefundPercent = model.RefundPercent;
+            await _context.SaveChangesAsync();
+            return exist;
+        }
+    }
+}
