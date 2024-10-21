0d264f486b7f7c1de981a945ce4910994cefae92
Boisopsop <nhathoang1645@gmail.com>
2024-10-21 08:30:23 +0700

    create link google meet when create booking

---
+++ KoiFishCare/Models/GoogleCalendar.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoiFishCare.Models
+{
+    public class GoogleCalendar
+    {
+        public string Summary {get;set;}
+        public string Description {get;set;}
+        public string Location{get; set;}
+        public DateTime Start {get;set;}
+        public DateTime End {get; set;}
+    }
+}
