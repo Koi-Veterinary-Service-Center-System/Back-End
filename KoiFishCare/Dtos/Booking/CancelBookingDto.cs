3e9d9058e2094f5d357843c35ca42dd42cfa7a82
ntpan04 <nguyenan19101112@gmail.com>
2024-10-31 21:13:46 +0700

    add range to refundpercent in cancelbookingdto

--- KoiFishCare/Dtos/Booking/CancelBookingDto.cs
+++ KoiFishCare/Dtos/Booking/CancelBookingDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,8 @@ namespace KoiFishCare.Dtos.Booking
         public string? BankName { get; set; }
         public string? CustomerBankNumber { get; set; }
         public string? CustomerBankAccountName { get; set; }
+        
+        [Range(0,100)]
         public int? RefundPercent { get; set; }
     }
 }
