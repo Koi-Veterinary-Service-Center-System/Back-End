b92f4c93e40a839a9436b1061b8876a1e6d7b996
BeoChu <chouladila@gmail.com>
2024-11-03 16:01:49 +0700

    Fix view attribute IsDeleted in payment

--- KoiFishCare/Mappers/PaymentMappers.cs
+++ KoiFishCare/Mappers/PaymentMappers.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using KoiFishCare.Dtos.Payment;
+using KoiFishCare.DTOs.Payment;
 using KoiFishCare.Models;
 
 namespace KoiFishCare.Mappers
 {
     public static class PaymentMappers
     {
-        public static Payment ToModelFromDTO(this AddPaymentDTO addPaymentDTO)
+        public static PaymentDTO ToDTOFromModel(this Payment payment)
         {
-            return new Payment
-            {
-                Type = addPaymentDTO.Type,
+            return new PaymentDTO {
+                PaymentID = payment.PaymentID,
+                Type = payment.Type,
+                IsDeleted = payment.IsDeleted,
             };
         }
     }
